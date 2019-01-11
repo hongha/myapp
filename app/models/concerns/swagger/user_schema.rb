742cadd7b7d43d3ae566167d27c2a5daa6a54623
doanhongha <doanhong.ha@monstar-lab.com>
2019-01-11 19:27:51 +0900

    first commit

--- app/models/concerns/swagger/user_schema.rb
+++ app/models/concerns/swagger/user_schema.rb
@@ -3,8 +3,22 @@ module Swagger::UserSchema
   include Swagger::Blocks
 
   included do
-    swagger_schema :User do
+    swagger_schema :UserInput do
       key :required, [:name, :email]
+      property :name do
+        key :type, :string
+      end
+      property :email do
+        key :type, :string
+      end
+    end
+
+    swagger_schema :UserOutput do
+      key :required, [:id, :name, :email]
+      property :id do
+        key :type, :integer
+        key :format, :int64
+      end
       property :name do
         key :type, :string
       end
